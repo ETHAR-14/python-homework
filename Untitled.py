02b23d1d5fd4d3fa4b2a80be76f33cf85818a8f9
ETHAR-14 <aither201w2@gmail.com>
2026-01-29 19:21:28 +0300

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+name =" Sara"  
+
+
+# In[5]:
+
+
+age=20
+
+
+# In[6]:
+
+
+height=165.5
+
+
+# In[7]:
+
+
+is_student=True
+
+
+# In[8]:
+
+
+info={"major":"CS","year":2}
+
+
+# In[9]:
+
+
+print(name,type(name))
+
+
+# In[10]:
+
+
+print(age,type(age))
+
+
+# In[11]:
+
+
+print(height,type(height))
+
+
+# In[12]:
+
+
+print(is_student,type(is_student))
+
+
+# In[16]:
+
+
+print("grades",type("grades"))
+
+
+# In[18]:
+
+
+print("intfo",type("intfo"))
+
+
+# In[ ]:
+
+
+
+
