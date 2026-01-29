e86633851ffffeee52ba4f67f24c3a6afb7362eb
ETHAR-14 <aither201w2@gmail.com>
2026-01-29 19:19:37 +0300

    Add files via upload

---
+++ Untitled (1).py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print ("welcome to python programing!")
+
+
+
+# In[4]:
+
+
+name="shmaeyl"
+
+
+# In[5]:
+
+
+print (name)
+
+
+# In[6]:
+
+
+cors="python"
+
+
+# In[7]:
+
+
+print(cors)
+
+
+# In[8]:
+
+
+#welcom to python
+
+
+# In[9]:
+
+
+student_name="Aice"
+
+
+# In[10]:
+
+
+age=20
+
+
+# In[11]:
+
+
+student_name="Alice"
+
+
+# In[12]:
+
+
+gpa=3.75
+
+
+# In[13]:
+
+
+subjects=["math" ,"python","phsics"]
+
+
+# In[14]:
+
+
+student_intfo={"id":101,"year":2}
+
+
+# In[16]:
+
+
+skills={"python"," SQL"}
+
+
+# In[18]:
+
+
+is_enrolled=True
+
+
+# In[19]:
+
+
+gradution_date=None
+
+
+# In[20]:
+
+
+print(student_name,type(student_name))
+
+
+# In[21]:
+
+
+print(age,type(age))
+
+
+# In[22]:
+
+
+print(gpa,type(gpa))
+
+
+# In[23]:
+
+
+print(subjects,type(subjects))
+
+
+# In[25]:
+
+
+print('stduent_info',type(student_intfo))
+
+
+# In[27]:
+
+
+print('skills',type(skills))
+
+
+# In[28]:
+
+
+print('is_enrolled',type(is_enrolled))
+
+
+# In[37]:
+
+
+print(":graduation_date:", type ('graduation_date'))
+
+
+# In[ ]:
+
+
+
+
